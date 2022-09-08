23b6330a96784b6cdad77a557d5ec8fa7f447968
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:39:09 +0430

    add project data

---
+++ src/component/header/SocialLinks.js
@@ -0,0 +1,17 @@
+import { ReactComponent as GitHub } from "../../images/icons/github.svg";
+import { ReactComponent as AngelList } from "../../images/icons/angellist.svg";
+import { ReactComponent as LinkedIn } from "../../images/icons/linkedin.svg";
+import { ReactComponent as Twitter } from "../../images/icons/twitter.svg";
+
+export const socialLinks = [
+  { id: 0, icon: GitHub, link: "https://github.com/samiullahbahadur" },
+  { id: 1, icon: AngelList, link: "https://angel.co/u/samiullah-bahadur" },
+  {
+    id: 2,
+    icon: LinkedIn,
+    link: "https://www.linkedin.com/in/samiullah-bahadur/",
+  },
+  { id: 3, icon: Twitter, link: "https://twitter.com/Samiull88496331" },
+];
+
+export default "SocialLinks";
---
+++ src/component/project/ProjectData.js
@@ -0,0 +1,41 @@
+import budgetApp from "../../images/projects/budgetApp.png";
+import covid from "../../images/projects/covid.png";
+import leaderboard from "../../images/projects/leaderboard.png";
+import TV from "../../images/projects/TV.png";
+
+export const projects = [
+  {
+    id: 0,
+    status: "Development",
+    title: "BudgetApp",
+    img: budgetApp,
+    projectLink: "https://github.com/samiullahbahadur/budget-app",
+    liveLink: "https://budget1-app.herokuapp.com/splashs/index",
+  },
+  {
+    id: 1,
+    status: "Development",
+    title: "Covid-tracker",
+    img: covid,
+    projectLink: "https://github.com/samiullahbahadur/Covid-tracker",
+    liveLink: "https://cvdotracker.herokuapp.com/",
+  },
+  {
+    id: 2,
+    status: "Development",
+    title: "LeaderBoard",
+    img: leaderboard,
+    projectLink: "https://github.com/samiullahbahadur/LeaderBoard",
+    liveLink: "https://dev--amazing-semifreddo-98cee0.netlify.app/",
+  },
+  {
+    id: 3,
+    status: "Development",
+    title: "TV-series-and-movies",
+    img: TV,
+    projectLink: "https://github.com/samiullahbahadur/TV-series-and-movies",
+    liveLink: "https://tv-maize-zahra-pascal-samiullah-jscapstone.netlify.app/",
+  },
+];
+
+export default "Project Data";
