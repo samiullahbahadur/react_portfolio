90571902de1d7d5795725a61f6bd79384b97a7a5
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:37:56 +0430

    add about data

---
+++ src/component/about/AboutData.js
@@ -0,0 +1,62 @@
+export const aboutLinks = [
+  { id: 1, link: "Main Skills" },
+  { id: 2, link: "Experience" },
+  { id: 3, link: "Education & Certificate" },
+];
+
+export const aboutDetails = [
+  [
+    {
+      id: 1,
+      title: "Front-End Developer",
+      extraTitle: "",
+      description: "JavaScript, React, Redux, HTML5, CSS3",
+    },
+    {
+      id: 2,
+      title: "Back-End Developer",
+      extraTitle: "",
+      description: "Ruby, Rails, PostgreSQL",
+    },
+    {
+      id: 3,
+      title: "Professional",
+      extraTitle: "",
+      description:
+        "Remote collaboration, Pair-Programming, cross-communication,Teamwork, Mentoring, Agile Methodologies",
+    },
+  ],
+  [
+    {
+      id: 1,
+      title: "Integrated Financial Management System (IFMS) specialist",
+      extraTitle:
+        " Independent Directorate of Local Governance (IDLG) · Full-time",
+      description:
+        "Jun 2019 - Present ,Financial Management System Development ,  Performed Surveys and System based assessment, municipality data gathering, manipulation and analyzing for IFMS in IDLG",
+    },
+    {
+      id: 2,
+      title: "IT Officer",
+      extraTitle: " - Afghan Cyber ISP · Full-time",
+      description:
+        "Jan 2018 - Jun 2019 · 1 yr 6 mos, Apply and used new technologies ,  Wireless, Vast, Microware, Fib Optic , IT Clinic, Network Services, web Design and hosting ",
+    },
+  ],
+  [
+    {
+      id: 1,
+      title: "Remote Full Stack Web Development Program, Full-Time",
+      extraTitle: " - Microverse",
+      description: "2022",
+    },
+    {
+      id: 2,
+      title: "Bachelor's degreeComputer Software Engineering, Full-Time",
+      extraTitle: " - Kabul polytchnic Unisersity(KPU)",
+      description: "2015",
+    },
+  ],
+];
+
+export default "Working";
