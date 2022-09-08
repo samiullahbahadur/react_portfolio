5224ad794a5375b4af04a418b0671da07b06adea
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:38:34 +0430

    add porfolio data

---
+++ src/component/project/Portfolio.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PageTitle from "../PageTitle";
+import "../../stylesheets/portfolio.css";
+import ProjectCard from "./ProjectCard";
+import { projects } from "./ProjectData";
+
+const Portfolio = () => {
+  const title = "My Latest Project";
+  const desc = "Checkout my latest JavaScript, React and Rails projects";
+
+  return (
+    <div className="projects-page">
+      <PageTitle title={title} desc={desc} />
+      <ul className="projects-container">
+        {projects.map((project, index) => (
+          <li
+            id={`project-container${index + 1}`}
+            className="project-container-all"
+            key={`project-${project.id}`}
+          >
+            <ProjectCard
+              status={project.status}
+              title={project.title}
+              projectLink={project.projectLink}
+              liveLink={project.liveLink}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Portfolio;
