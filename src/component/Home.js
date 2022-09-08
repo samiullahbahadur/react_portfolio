3d8555b41f08e3442523857f2cc96eccffc780d8
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:35:08 +0430

    Add home page

---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stylesheets/homepage.css";
+
+const Home = () => (
+  <div className="homepage-component">
+    <h3 className="greeting">WELCOME TO MY WORLD!</h3>
+    <h1 className="hero-title">
+      Hi, I&apos;m Samiullah
+      <br />
+      <span className="homepage-profession-title">.</span>
+      <br />
+      <span className="hero-title-two">working remotely.</span>
+    </h1>
+  </div>
+);
+
+export default Home;
