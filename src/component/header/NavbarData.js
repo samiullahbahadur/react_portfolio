ae74c1b77425d736f8bfefd6b77faa26bae510e5
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:38:16 +0430

    add navbar data

---
+++ src/component/header/NavbarData.js
@@ -0,0 +1,9 @@
+export const links = [
+  { id: "1", to: "#home", title: "Home" },
+  { id: "2", to: "#about", title: "About" },
+  { id: "3", to: "#services", title: "Services" },
+  { id: "4", to: "#portfolio", title: "Portfolio" },
+  { id: "5", to: "#contact", title: "Contact" },
+];
+
+export default "NavbarData";
