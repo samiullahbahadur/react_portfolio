9f47b115d44022b6706d7f74ec612c5a5aaa8bbe
samiullah <bahadursamiullah@gmail.com>
2022-09-08 17:33:20 +0430

    UPdate app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/header/Navbar";
+import Home from "./component/Home";
+import About from "./component/about/About";
+import Services from "./component/service/Services";
+import Portfolio from "./component/project/Portfolio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Contact from "./component/Contact";
+
+const App = () => (
+  <>
+    <header>
+      <Navbar />
+    </header>
+    <section id="home">
+      <Home />
+    </section>
+    <section id="about">
+      <About />
+    </section>
+    <section id="services">
+      <Services />
+    </section>
+    <section id="portfolio">
+      <Portfolio />
+    </section>
+
+    <section id="contact">
+      <Contact />
+    </section>
+  </>
+);
 
 export default App;
